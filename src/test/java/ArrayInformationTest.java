f4e66c88a8f2cec3645f36d32d16ce39dbc93c99
Rachel Trana <raxytrana@gmail.com>
2021-02-02 21:43:23 -0600

    Updating tests for left and right boundaries

--- src/test/java/ArrayInformationTest.java
+++ src/test/java/ArrayInformationTest.java
@@ -38,6 +38,27 @@ public class ArrayInformationTest {
     }*/
 
     /*@Test
+    void findLeftBoundary_evenSizedMiddleLeft() {
+        int[] array = { 1, 2, 3, 4};
+        ArrayInformation aInfo = new ArrayInformation(array);
+        assertEquals(2, aInfo.findLeftBoundary(2));
+    }*/
+
+    /*@Test
+    void findLeftBoundary_evenSizedMiddleRight() {
+        int[] array = { 1, 2, 3, 4};
+        ArrayInformation aInfo = new ArrayInformation(array);
+        assertEquals(3, aInfo.findLeftBoundary(3));
+    }*/
+
+    /*@Test
+    void findLeftBoundary_oddSizedMiddle() {
+        int[] array = { 1, 2, 3, 4, 5};
+        ArrayInformation aInfo = new ArrayInformation(array);
+        assertEquals(3, aInfo.findLeftBoundary(3));
+    }*/
+
+    /*@Test
     void findRightBoundary_shouldReturnRightBoundaryIfExists() {
         int[] array = { 1, 9, 4, 8, 6 };
         ArrayInformation aInfo = new ArrayInformation(array);
@@ -56,5 +77,26 @@ public class ArrayInformationTest {
         int[] array = { 15, 9, 4, 8, 6, 10, 20, 9};
         ArrayInformation aInfo = new ArrayInformation(array);
         assertEquals(9, aInfo.findRightBoundary(9));
+    }*/
+
+    /*@Test
+    void findRightBoundary_evenSizedMiddleLeft() {
+        int[] array = { 1, 2, 3, 4};
+        ArrayInformation aInfo = new ArrayInformation(array);
+        assertEquals(2, aInfo.findRightBoundary(2));
+    }*/
+
+    /*@Test
+    void findRightBoundary_evenSizedMiddleRight() {
+        int[] array = { 1, 2, 3, 4};
+        ArrayInformation aInfo = new ArrayInformation(array);
+        assertEquals(3, aInfo.findRightBoundary(3));
+    }*/
+
+    /*@Test
+    void findRightBoundary_oddSizedMiddle() {
+        int[] array = { 1, 2, 3, 4, 5};
+        ArrayInformation aInfo = new ArrayInformation(array);
+        assertEquals(3, aInfo.findRightBoundary(3));
     }*/
 }
